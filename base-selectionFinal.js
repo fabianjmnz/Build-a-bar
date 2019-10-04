b0a6e1a7eecd447b76dd19e5124eaf0809d97574
Fabian Jimenez <efjmnz@gmail.com>
2019-10-04 12:34:37 -0500

    Final Project

--- base-selectionFinal.js
+++ base-selectionFinal.js
@@ -54,9 +54,20 @@ function getDrinks(keyword) {
                 var arb = [newlist[a].drinks[0].strIngredient1,newlist[a].drinks[0].strIngredient2,newlist[a].drinks[0].strIngredient3,newlist[a].drinks[0].strIngredient4,newlist[a].drinks[0].strIngredient5, newlist[a].drinks[0].strIngredient6,newlist[a].drinks[0].strIngredient7,newlist[a].drinks[0].strIngredient8, newlist[a].drinks[0].strIngredient9,newlist[a].drinks[0].strIngredient10];
 
                 document.getElementById(`${a}`).onclick = function(){
-                    document.getElementById("drink-list").style.display = "none";
-                    $("section2").append(ar);
-		
+                    document.getElementById("drink-list").style.display = "none";	
+                    var tu=[];
+                if(keyword=='tequila')	{
+                    tu = [96,169,573,422,394,74,183,36,'NA','NA',276,57,84,166,613,168,193,325,136,200,96,200,'NA',103]
+                }
+                if(keyword=='rum'){
+                    tu = [573,'NA',1228,314,739,'NA','NA',112,'NA',203,'NA',613,187,323,128,783,343]
+                }
+                if(keyword=='bourbon'){
+                    tu = [119,178,'NA',120,184,183,215,187,236,156,147,154]
+                }
+                if(keyword=='vodka'){
+                    tu = [172,279,,182,239,'NA',85,'NA','NA',573,'NA','NA',249,169,'NA',503,318,189,'NA',144,'NA',248,173,72,226,181,140,143,52,9588,133,380,'NA','NA',126,111,151,285,205,138,176,324,195,207,87,201,80,208,337,83,'NA',214,85,60,276,276,97,182,203,325,'NA',820,'NA',197,256,613,178,244,543,84,180,326,221,190,183,175,106,202,500,500,'NA',112]
+                }
                     firstIngredient1 = arb[0];
                     secondIngredient2 = arb[1];
                     thirdIngredient3 = arb[2];
@@ -67,58 +78,61 @@ function getDrinks(keyword) {
                     eightIngredient8 = arb[7];
                     ninthIngredient9 = arb[8];
                     tenthIngredient10 = arb[9];
-
-		
-			var nameOfDrink = $("<h1>").text(newlist[a].strDrink);     // Name of Drink
-			$("body").append(nameOfDrink);
-			var firstIngredient = $("<h3>").text(newlist[a].drinks[0].strMeasure1+ " " + firstIngredient1);  // Name of Ingredient 1 if needed
-			$("body").append(firstIngredient);
+                    var cal = (`Calories: ${tu[a]} `)
+                    console.log(cal)
+            $("section2").append(`<img src="images/alcohol-alcoholic-beverage-bar-602750.jpg" >`);        	
+			var nameOfDrink = $("<h1>").text(newlist[a].drinks[0].strDrink);     // Name of Drink
+			$("section2").append(nameOfDrink);
+			var firstIngredient = $("<h4>").text(newlist[a].drinks[0].strMeasure1+ " " + firstIngredient1);  // Name of Ingredient 1 if needed
+			$("section2").append(firstIngredient);
 		    var secondIngredient = $("<h4>").text(newlist[a].drinks[0].strMeasure2+ " " + secondIngredient2);  // Name of Ingredient 2 if needed
-			$("body").append(secondIngredient);
+			$("section2").append(secondIngredient);
 		if (isNaN(newlist[a].drinks[0].strMeasure3)){
 			newlist[a].drinks[0].strMeasure3 =  '' ;
 			var thirdIngredient = $("<h4>").text(newlist[a].drinks[0].strMeasure3+ " " + thirdIngredient3);    // Name of Ingredient 3 if needed
-			$("body").append(thirdIngredient);
+			$("section2").append(thirdIngredient);
 		}
 		if (isNaN(newlist[a].drinks[0].strMeasure4)){	
 			newlist[a].drinks[0].strMeasure4 = '' ;
 			var fourthIngredient = $("<h5>").text(newlist[a].drinks[0].strMeasure4+ " " + fourthIngredient4);   // Name of Ingredient 4 if needed
-			$("body").append(fourthIngredient);	
+			$("seciton2").append(fourthIngredient);	
 		}	    
 		if(isNaN(newlist[a].drinks[0].strMeasure5)){
 			newlist[a].drinks[0].strMeasure5 = '' ;
 		    var fifthIngredient = $("<h4>").text(newlist[a].drinks[0].strMeasure5+ " " + fifthIngredient5);     // Name of Ingredient 5 if needed
-			$("body").append(fifthIngredient);
+			$("section2").append(fifthIngredient);
 		}
 		if(isNaN(newlist[a].drinks[0].strMeasure6)){
 			newlist[a].drinks[0].strMeasure6 = '' ;
 		    var sixthIngredient = $("<h4>").text(newlist[a].drinks[0].strMeasure6+ " " + sixthIngredient6);    // Name of Ingredient 6 if needed
-			$("body").append(sixthIngredient);
+			$("section2").append(sixthIngredient);
 		}
 		if(isNaN(newlist[a].drinks[0].strMeasure7)){
 			newlist[a].drinks[0].strMeasure7 = '';
 		    var seventhIngredient = $("<h5>").text(newlist[a].drinks[0].strMeasure7+ " " + seventhIngredient7);   // Name of Ingredient 7 if needed
-			$("body").append(seventhIngredient);
+			$("section2").append(seventhIngredient);
 		}
 		if(isNaN(newlist[a].drinks[0].strMeasure8)){
 			newlist[a].drinks[0].strMeasure8 = '';
 		    var eightIngredient = $("<h4>").text(newlist[a].drinks[0].strMeasure8+ " " + eightIngredient8);     // Name of Ingredient 8 if needed
-			$("body").append(eightIngredient);
+			$("section2").append(eightIngredient);
 		}
 		if(isNaN(newlist[a].drinks[0].strMeasure9)){
 			newlist[a].drinks[0].strMeasure9 = '';
 		    var seventhIngredient = $("<h5>").text(newlist[a].drinks[0].strMeasure9+ " " + seventhIngredient9);   // Name of Ingredient 9 if needed
-			$("body").append(ninthIngredient);
+			$("section2").append(ninthIngredient);
 		}
 		if(isNaN(newlist[a].drinks[0].strMeasure10)){
 			newlist[a].drinks[0].strMeasure10 = '';
 		    var eightIngredient = $("<h4>").text(newlist[a].drinks[0].strMeasure10+ " " + eightIngredient10);     // Name of Ingredient 10 if needed
-			$("body").append(tenthIngredient);
+			$("section2").append(tenthIngredient);
 		}
 			var recipe = $("<h4>").text(newlist[a].drinks[0].strInstructions);     // Name of Ingredient 8 if needed
-			$("body").append(recipe);
-			
-			//allName.push(dict[i].strDrink)
+			$("section2").append(recipe);
+            
+            var calz = $("<h4>").text(cal)
+            $("section2").append(calz);
+	
 
                 }       
          })//end of foreach
